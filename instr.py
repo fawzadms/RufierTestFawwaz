5ead76d26ff1a66096eb0c8874ea8f0b6fdaac52
georendy <georendy@gmail.com>
2024-07-05 20:45:12 +0700

    Update instr.py

--- instr.py
+++ instr.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import QTime
 
 win_x, win_y = 200, 100
 win_width, win_height = 1000, 600
-
+#ini hasil editan saya
 txt_hello = 'Welcome to the Health status detection program!'
 txt_next = 'Start'
 txt_instruction = ('This application allows you to use the Rufier test to make an initial diagnosis of your health.\n'
